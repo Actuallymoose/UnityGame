66d8619bd3419f7362bb5305178390b26c661ebe
Manu <37301784+Actuallymoose@users.noreply.github.com>
2018-11-05 10:21:55 +0800

    Added Initial Project
    Basic player with mouselook, movement and jumping

---
+++ Game Mechanics/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    public string mouseXInputName, mouseYInputName;
+
+    [Range(0,10)]
+    public float sensitivity;
+    public Transform player;
+
+    float clampY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        LockCursor();
+        clampY = 0.0f;
+	}
+
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CameraRotation();
+	}
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxisRaw(mouseXInputName) * sensitivity;
+        float mouseY = Input.GetAxisRaw(mouseYInputName) * sensitivity;
+
+        clampY += mouseY;
+
+        if(clampY > 60.0f)
+        {
+            clampY = 60.0f;
+            mouseY = 0.0f;
+        }
+        else if(clampY < -60.0f)
+        {
+            clampY = -60.0f;
+            mouseY = 0.0f;
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        player.Rotate(Vector3.up * mouseX);
+    }
+    
+}
---
+++ Game Mechanics/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float forwardSpeed, strafeSpeed, jumpMultiplier; // force variables
+    public string horizontalInputName, verticalInputName; // variables to hold movement keys - see unity explorer
+
+    // jumping variables
+    public AnimationCurve jumpFallOff; // handles the curve of the jump
+    public KeyCode jumpKey;
+    bool isJumping;
+
+    CharacterController charControl; // character controller object - used to rotate the player instead of camera
+
+    // Use this for initialization
+    void Start ()
+    {
+        charControl = GetComponent<CharacterController>();
+        isJumping = false;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+   void Movement()
+    {
+        // time.deltaTime not needed because simple move does it for you
+        float horizInput = Input.GetAxis(horizontalInputName) * strafeSpeed; // left and right - ad
+        float vertInput = Input.GetAxis(verticalInputName) * forwardSpeed; // forward and back - ws
+
+        Vector3 forwardMove = transform.forward * vertInput; // vector that stores the current movement in forward/back direction
+        Vector3 rightMove = transform.right * horizInput; // vector that stores the current movement in left/right direction
+
+        charControl.SimpleMove(forwardMove + rightMove); // moves the player according to direction vectors - applies time.deltaTime
+    }
+
+    // decides when the player wants to jump
+    void Jump()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    // performs the actual jump animation
+    IEnumerator JumpEvent()
+    {
+        charControl.slopeLimit = 90.0f; // stops wierd clipping issues when jumping against obstacles
+        float timeInAir = 0.0f;
+
+        // loops until player hits roof or ground - causes the player to jump in an arc
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charControl.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charControl.isGrounded && charControl.collisionFlags != CollisionFlags.Above);
+
+        charControl.slopeLimit = 45.0f; // as above
+        isJumping = false;
+    }
+
+}
