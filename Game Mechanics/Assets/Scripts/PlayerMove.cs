c4ce7670d1d309dec4335eff97d9f66bb3976552
Manu <37301784+Actuallymoose@users.noreply.github.com>
2018-11-29 07:24:46 +0800

    Adjusted values

--- Game Mechanics/Assets/Scripts/PlayerMove.cs
+++ Game Mechanics/Assets/Scripts/PlayerMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [Range(0, 10)]
-    public float speed, jumpMultiplier, sprintSpeed, airmodifier = 0.5f; // force variables
+    public float speed = 6f, jumpMultiplier = 6f, sprintSpeed = 2f, airmodifier = 0.5f; // force variables
     public string horizontalInputName, verticalInputName, jumpInputName, sprintInputName; // variables to hold movement keys - see unity explorer
 
     // jumping variables
@@ -75,8 +75,9 @@ public class PlayerMove : MonoBehaviour
         {
             float jumpForce = jumpFallOff.Evaluate(timeInAir);
 
+            float vertInput = Input.GetAxis(verticalInputName) * speed * airmodifier;
             float horizInput = Input.GetAxis(horizontalInputName) * speed * airmodifier; // left and right - ad
-            Vector3 move = transform.right * horizInput + Vector3.up * jumpForce * jumpMultiplier;
+            Vector3 move = transform.right * horizInput + transform.forward * vertInput + Vector3.up * jumpForce * jumpMultiplier;
 
             charControl.Move(move * Time.deltaTime);
             timeInAir += Time.deltaTime;
