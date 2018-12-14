d6ec9bb4b4624ffa8aa2ca65d721303c07969e2f
Manu <37301784+Manubm97@users.noreply.github.com>
2018-12-14 11:00:59 +0800

    Started on ai stuff

---
+++ Game Mechanics/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    enum ENEMY_STATES {PATROL, ATTACK}
+    ENEMY_STATES state;
+    
+    public float lookRadius = 10f, slerpSmoothing = 5f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(target.position, transform.position); // distance between enemy and player
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if(distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+	}
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * slerpSmoothing);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
