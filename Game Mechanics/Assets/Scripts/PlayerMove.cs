8957e6f2897ac94f311f1050001f26191547b9b1
Manu <37301784+Manubm97@users.noreply.github.com>
2018-12-14 09:51:28 +0800

    Added actual slowed movement after jumping
    Slope method is broken now tho

--- Game Mechanics/Assets/Scripts/PlayerMove.cs
+++ Game Mechanics/Assets/Scripts/PlayerMove.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [Range(0, 10)]
-    public float speed = 6f, sprintSpeed = 2f, airModifier = 0.25f; // force variables
+    public float speed = 6f, sprintSpeed = 2f, airSpeed = 3f; // force variables
+
     public string horizontalInputName, verticalInputName, jumpInputName, sprintInputName; // variables to hold movement keys - see unity explorer
 
-    public float gravity = 14f, jumpForce = 10f, verticalVelocity;
-
-    [Range(0,10)]
+    public float gravity = 14f, jumpForce = 10f; // jump variables
     public float slopeRayLengthMultiplyer, slopeDownwardForce;// slope variables
+
+    private Vector3 jumpVelocity = new Vector3(); // for jumping movement
 
     CharacterController controller; // character controller object - used to rotate the player instead of camera
 
@@ -24,59 +25,41 @@ public class PlayerMove : MonoBehaviour
 	// Update is called once per frame
     void Update()
     {
-        Movement();
-    }
-
-   void Movement()
-    {
-        float timer = 0;
-
-        float horizInput = Input.GetAxis(horizontalInputName);
-        float vertInput = Input.GetAxis(verticalInputName);
-        Vector3 startVelocity = transform.right * horizInput + transform.forward * vertInput;
-        startVelocity *= speed;
-
-        Vector3 airTargetVector = new Vector3();
+        Vector3 move = new Vector3(Input.GetAxis(horizontalInputName), 0, Input.GetAxis(verticalInputName));
+        move = transform.TransformDirection(move);
 
         if (controller.isGrounded)
         {
-            airTargetVector = startVelocity;
+            move *= speed;
 
             // enables shift only in the forward direction
             if (Input.GetButton(sprintInputName) && Input.GetAxis(verticalInputName) > -0)
             {
-                startVelocity *= sprintSpeed;
+                move *= sprintSpeed;
             }
 
             // jumps
             if (Input.GetButton(jumpInputName))
             {
-                verticalVelocity = jumpForce;
-                timer = 0;
+                jumpVelocity = move;
+                jumpVelocity.y = jumpForce;
             }
-
-            airTargetVector = new Vector3();
+            else
+            {
+                jumpVelocity = new Vector3();
+            }
         }
         else
         {
-            airTargetVector = startVelocity;
-            airTargetVector += transform.right * Input.GetAxis(horizontalInputName) * airModifier + transform.forward * Input.GetAxis(verticalInputName) * airModifier;
+            move *= airSpeed;
         }
 
-        timer += Time.deltaTime * 0.5f;
-        Debug.Log(timer);
+        jumpVelocity.y -= gravity * Time.deltaTime; // applies gravity
 
-        verticalVelocity -= gravity * Time.deltaTime; // applies gravity
-
-        startVelocity += Vector3.up * verticalVelocity;
-
-        Vector3 finalMove = Vector3.Lerp(startVelocity, airTargetVector, timer);
-
-        controller.Move(finalMove * Time.deltaTime);
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        controller.Move((move + jumpVelocity) * Time.deltaTime);
+       
         // if moving and on slope - move player down so they can move down slopes without falling
-        if ((vertInput != 0 || horizInput != 0) && OnSlope())
+        if ((Input.GetAxis(verticalInputName) != 0 || Input.GetAxis(horizontalInputName) != 0) && OnSlope())
         {
             controller.Move(Vector3.down * controller.height / 2 * slopeDownwardForce * Time.deltaTime);
         }
@@ -85,7 +68,7 @@ public class PlayerMove : MonoBehaviour
     // returns true if player is on a slope
     bool OnSlope()
     {
-        if(!controller.isGrounded)
+        if(!controller.isGrounded) // needs to be positive instead
         {
             return false;
         }
