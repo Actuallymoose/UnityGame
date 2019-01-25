c3c822bb3a0402ee84ca39598457f8afacbf25f2
Manu <37301784+Manubm97@users.noreply.github.com>
2019-01-25 11:15:58 +0800

    d

--- Game Mechanics/Assets/Scripts/Player/HackPlayer.cs
+++ Game Mechanics/Assets/Scripts/Player/HackPlayer.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class HackPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    string buttonChoice;
+    PlayerMove moveVariables;
+
+    public int increment = 1;
+
+    private void Start()
+    {
+        moveVariables.GetComponent<PlayerMove>();
+    }
+
+    private void Update()
+    {
+        
+
+        switch (buttonChoice)
+        {
+            case "increaseSpeed":
+                moveVariables.speed += increment;
+                break;
+            case "decreaseSpeed":
+                moveVariables.speed -= increment;
+                break;
+        }
+    }
+
+    public void GetButtonChoice(string choice)
+    {
+        buttonChoice = choice;
+    }
+
 }
