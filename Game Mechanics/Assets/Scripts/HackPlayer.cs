c6b20ae0f3d0b0ae37bf37fcbf3b481496cbaf64
Manu <37301784+Manubm97@users.noreply.github.com>
2019-02-02 09:35:23 +0800

    Finished player hacking

--- Game Mechanics/Assets/Scripts/HackPlayer.cs
+++ Game Mechanics/Assets/Scripts/HackPlayer.cs
@@ -8,7 +8,7 @@ public class HackPlayer : MonoBehaviour {
     string buttonChoice;
     public PlayerMove moveVariables;
     public PlayerHackResources points;
-    public Text speedText;
+    public Text speedText, gravityText, jumpText, airControlText, sprintText;
 
     public float increment = 1f;
 
@@ -31,22 +31,61 @@ public class HackPlayer : MonoBehaviour {
                 }
                 break;
             case "+gravity":
+                if(points.currentPoints > 0)
+                {
+                    AlterGravity(increment);
+                    points.AlterPoints(-1);
+                }
                 break;
             case "-gravity":
+                if(moveVariables.gravity > 1f)
+                {
+                    AlterGravity(-increment);
+                    points.AlterPoints(1);
+                }
                 break;
             case "+jump":
+                if(points.currentPoints > 0)
+                {
+                    AlterJump(increment);
+                    points.AlterPoints(-1);
+                }
                 break;
             case "-jump":
+                if(moveVariables.jumpForce > 1f)
+                {
+                    AlterJump(-increment);
+                    points.AlterPoints(1);
+                }
                 break;
             case "+airMove":
+                if(points.currentPoints > 0)
+                {
+                    AlterAirMove(increment);
+                    points.AlterPoints(-1);
+                }
                 break;
             case "-airMove":
+                if(moveVariables.airSpeed > 1f)
+                {
+                    AlterAirMove(-increment);
+                    points.AlterPoints(1);
+                }
                 break;
             case "+sprint":
+                if(points.currentPoints > 0)
+                {
+                    AlterSprint(increment);
+                    points.AlterPoints(-1);
+                }
                 break;
             case "-sprint":
+                if(moveVariables.sprintSpeed > 1f)
+                {
+                    AlterSprint(-increment);
+                    points.AlterPoints(1);
+                }
                 break;
-
         }
 
         UpdateText();
@@ -63,8 +102,32 @@ public class HackPlayer : MonoBehaviour {
         moveVariables.speed += amount;
     }
 
+    void AlterGravity(float amount)
+    {
+        moveVariables.gravity += amount;
+    }
+
+    void AlterJump(float amount)
+    {
+        moveVariables.jumpForce += amount;
+    }
+
+    void AlterAirMove(float amount)
+    {
+        moveVariables.airSpeed += amount;
+    }
+
+    void AlterSprint(float amount)
+    {
+        moveVariables.sprintSpeed += amount;
+    }
+
     void UpdateText()
     {
         speedText.text = "Player.speed = " + moveVariables.speed.ToString();
+        gravityText.text = "Player.gravity = " + moveVariables.gravity.ToString();
+        jumpText.text = "Player.jumpForce = " + moveVariables.jumpForce.ToString();
+        airControlText.text = "Player.airControl = " + moveVariables.airSpeed.ToString();
+        sprintText.text = "Player.sprintSpeed = " + moveVariables.sprintSpeed.ToString();
     }
 }
